5b68c9a4e1648039d0872278df1e312e2baa323a
Felipe Bezerra <felipe.bezerra@live.com>
2019-10-18 17:03:40 +0100

    Email HTML

--- src/main/java/com/felipebezerra/cursomc/servicies/MockEmailService.java
+++ src/main/java/com/felipebezerra/cursomc/servicies/MockEmailService.java
@@ -1,11 +1,13 @@
 package com.felipebezerra.cursomc.servicies;
 
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
-public class MockEmailService extends AbistractEmailService{
+public class MockEmailService extends AbstractEmailService{
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
 	
@@ -15,6 +17,13 @@ public class MockEmailService extends AbistractEmailService{
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");		
 	}
 
 }
