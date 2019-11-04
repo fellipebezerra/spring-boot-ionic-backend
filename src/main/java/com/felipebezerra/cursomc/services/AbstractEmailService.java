abfe8a32e7adca47898b3e57470a06af5692b874
fellipebezerra <felipe.bezerra@live.com>
2019-11-04 16:21:22 +0000

    colocando senha no properties

--- src/main/java/com/felipebezerra/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/felipebezerra/cursomc/services/AbstractEmailService.java
@@ -2,16 +2,8 @@ package com.felipebezerra.cursomc.services;
 
 import java.util.Date;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 import com.felipebezerra.cursomc.domain.Cliente;
 import com.felipebezerra.cursomc.domain.Pedido;
@@ -21,12 +13,6 @@ public abstract class AbstractEmailService implements EmailService {
 	
 	@Value("${default.sender}")
 	private String sender;
-	
-	@Autowired
-	private TemplateEngine templateEngine;
-	
-	//@Autowired
-	private JavaMailSender javaMailSender;
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
@@ -44,37 +30,9 @@ public abstract class AbstractEmailService implements EmailService {
 		return sm;
 	}
 	
-	protected String htmlFromTemplatePedido(Pedido obj) {
-		Context context = new Context();
-		context.setVariable("pedido", obj);
-		return templateEngine.process("email/confirmacaoPedido", context);
-	}
-	
-	@Override
-	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
-		try {
-			MimeMessage mm = prepareMimeMessageFromPedido(obj);
-			sendHtmlEmail(mm);
-		}
-		catch (MessagingException e) {
-			sendOrderConfirmationEmail(obj);
-		}
-	}
-
-	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
-		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
-		mmh.setTo(obj.getCliente().getEmail());
-		mmh.setFrom(sender);
-		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
-		mmh.setSentDate(new Date(System.currentTimeMillis()));
-		mmh.setText(htmlFromTemplatePedido(obj), true);
-		return mimeMessage;
-	}
-	
 	@Override
 	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
-		SimpleMailMessage sm = prepareNewPasswordEmail(cliente,newPass);
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
 		sendEmail(sm);
 	}
 
@@ -82,9 +40,9 @@ public abstract class AbstractEmailService implements EmailService {
 		SimpleMailMessage sm = new SimpleMailMessage();
 		sm.setTo(cliente.getEmail());
 		sm.setFrom(sender);
-		sm.setSubject("Solicitação de nova Senha ");
+		sm.setSubject("Solicitação de nova senha");
 		sm.setSentDate(new Date(System.currentTimeMillis()));
-		sm.setText("Nova Senha: " +newPass);	
+		sm.setText("Nova senha: " + newPass);
 		return sm;
 	}
 }
--- src/main/java/com/felipebezerra/cursomc/services/MockEmailService.java
+++ src/main/java/com/felipebezerra/cursomc/services/MockEmailService.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.felipebezerra.cursomc.domain.Pedido;
+
 public class MockEmailService extends AbstractEmailService{
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
@@ -24,6 +26,12 @@ public class MockEmailService extends AbstractEmailService{
 		LOG.info("Simulando envio de email HTML");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");		
+	}
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
